61dbf474cc0552e3301fa4af1f578a546eb3a6f3
Loknadh <loknadh.samanthula@wisig.com>
2026-03-24 16:47:03 +0530

    added new modules

---
+++ fsm.v
@@ -0,0 +1,20 @@
+module fsm(
+    input wire clk,
+    input wire rst,
+    input wire in,
+    output reg out
+);
+
+always@(posedge clk or posedge rst) begin
+    if (rst) begin
+        out <= 0;
+    end else begin
+        case (out)
+            0: out <= in; // Transition from state 0 to state 1 if input is 1
+            1: out <= in; // Stay in state 1 if input is 1, transition to state 0 if input is 0
+            default: out <= 0; // Default case to reset output
+        endcase
+    end
+end
+
+endmodule
