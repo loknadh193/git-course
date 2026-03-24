6a3513975d1e8c272666a7841d8935718a206d28
Loknadh <loknadh.samanthula@wisig.com>
2026-03-24 16:49:41 +0530

    added comment

--- fsm.v
+++ fsm.v
@@ -12,7 +12,7 @@ always@(posedge clk or posedge rst) begin
         case (out)
             0: out <= in; // Transition from state 0 to state 1 if input is 1
             1: out <= in; // Stay in state 1 if input is 1, transition to state 0 if input is 0
-            default: out <= 0; // Default case to reset output
+            default: out <= 0; // Default case to reset output  this should never be hit
         endcase
     end
 end
