8d95462ee6b2da3c92919d245d25ce8b186375a4
Loknadh <loknadh.samanthula@wisig.com>
2026-03-24 18:49:59 +0530

    new block

--- fsm.v
+++ fsm.v
@@ -17,4 +17,16 @@ always@(posedge clk or posedge rst) begin
     end
 end
 
+always@(posedge clk or posedge rst) begin
+    if (rst) begin
+        out <= 0;
+    end else begin
+        case (out)
+            0: out <= in; // Transition from state 0 to state 1 if input is 1
+            1: out <= in; // Stay in state 1 if input is 1, transition to state 0 if input is 0
+            default: out <= 0; // Default case to reset output  this should never be hit
+        endcase
+    end
+end
+
 endmodule
