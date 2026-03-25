e0937205920743254fe6c97a228104292850fa28
Loknadh <loknadh.samanthula@wisig.com>
2026-03-25 17:32:19 +0530

    registered comb outputs

--- fsm.v
+++ fsm.v
@@ -41,4 +41,16 @@ end
 
 // added combinational block to handle output logic
 
+always@(posedge clk or posedge rst) begin
+    if (rst) begin
+        out <= 0;
+    end else begin
+        case (out)
+            0: out <= in; // Transition from state 0 to state 1 if input is 1
+            1: out <= in; // Stay in state 1 if input is 1, transition to state 0 if input is 0
+            default: out <= 0; // Default case to reset output  this should never be hit
+        endcase
+    end
+end
+
 endmodule
